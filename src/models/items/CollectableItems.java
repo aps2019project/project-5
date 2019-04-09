724cf626e4fca262efcd1b366edf1c08a52ed9e9
mahdi5235 <mahdi.haji@yahoo.com>
2019-04-09 18:40:29 +0430

    Items package added

---
+++ src/models/items/CollectableItems.java
@@ -0,0 +1,8 @@
+package models.items;
+
+public class CollectableItems extends Item {
+    public enum CollectableTypes {
+    }
+    private CollectableTypes collectableTypes;
+
+}
---
+++ src/models/items/UsableItem.java
@@ -0,0 +1,6 @@
+package models.items;
+
+import models.cards.Card;
+
+public class UsableItem extends Card {
+}
