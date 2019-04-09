38d9f9abf26849245cc4cc2567ef889779cb9cfb
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-04-09 15:46:26 +0430

    create shop and change deck class

--- src/models/Deck.java
+++ src/models/Deck.java
@@ -1,4 +1,12 @@
 package models;
 
+import models.cards.Card;
+import java.util.ArrayList;
+
 public class Deck {
+    private ArrayList<Card> deck = new ArrayList<>();
+
+    public boolean isComplete() { return false; }
+    public void addCard(Card card) {}
+    public void removeCard(Card card) {}
 }
