c5bb575d7e8588a4920a58813b4dd2ea00aa3d0e
mahdi5235 <mahdi.haji@yahoo.com>
2019-04-15 17:18:51 +0430

    mahdi added account

--- src/views/Command.java
+++ src/views/Command.java
@@ -1,19 +1,10 @@
 package views;
 
-import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class Command {
     private Pattern pattern;
     private String functionName;
-    public static Scanner scanner = new Scanner(System.in);
-    public Pattern getPattern() {
-        return pattern;
-    }
-    public String getFunctionName() {
-        return functionName;
-    }
-    public void getCommand(){
-    }
-
+    public Pattern getPattern() { return pattern; }
+    public String getFunctionName() { return functionName; }
 }
