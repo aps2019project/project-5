92b31a7350c344950ee214f4addd7a0d52c701f2
mahdi5235 <mahdi.haji@yahoo.com>
2019-04-20 02:28:57 +0430

    account existence exception added

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,28 +1,30 @@
-package models.auth;
+package models;
 
-import models.Collection;
-import models.Deck;
-import models.MatchResult;
+import models.cards.Card;
 
 import java.util.*;
+class userNameExistsException extends Exception{
+    public userNameExistsException(){
+        super("username exists");
+    }
+}
 
 public class Account {
     private static Map<String, Account> accounts = new HashMap();
-
     private List<MatchResult> matchHistory;
     private String username;
     private String password;
     private Collection collection;
     private List<Deck> decks;
     private Deck deck;
-    private int drake=15000;
+    private int drake = 15000;
     private int winCount = 0;
-
+    public static userNameExistsException userNameExistsException=new userNameExistsException();
     public static final Comparator<Account> compare = Comparator.comparingInt(Account::getWinCount);
 
     public static Account getUser(String username, String password) {
-        for (Map.Entry<String , Account> entry: accounts.entrySet()){
-            if (entry.getValue().username.equals(username) && entry.getValue().password.equals(password)){
+        for (Map.Entry<String, Account> entry : accounts.entrySet()) {
+            if (entry.getValue().username.equals(username) && entry.getValue().password.equals(password)) {
                 return entry.getValue();
             }
 
@@ -30,8 +32,8 @@ public class Account {
         return null;
     }
 
-    public void incrementDrake(int drake){
-        this.drake+=drake;
+    public void incrementDrake(int drake) {
+        this.drake += drake;
     }
 
 
@@ -40,8 +42,19 @@ public class Account {
         this.password = password;
     }
 
-    public static void AddUser(Account user) {
-        accounts.putIfAbsent(user.username, user);
+    public static boolean doesAccountExists(Account account){
+        for (Map.Entry<String,Account> entry:accounts.entrySet()){
+            if (account.getUsername().equals(entry.getKey())){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void AddUser(Account user) throws userNameExistsException{
+        if (!doesAccountExists(user))
+            accounts.put(user.username, user);
+        else throw userNameExistsException;
     }
 
     public List<MatchResult> getMatchHistory() {
@@ -56,13 +69,18 @@ public class Account {
         return this.username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
     public ArrayList<Account> getRanking() {
-        ArrayList<Account> ranking = new ArrayList<Account>(accounts.values());
+        ArrayList<Account> ranking = new ArrayList<>(accounts.values());
         ranking.sort(compare);
         return ranking;
     }
 
-    public void addMatchResult(MatchResult matchResult){
+
+    public void addMatchResult(MatchResult matchResult) {
         matchHistory.add(matchResult);
     }
 }
