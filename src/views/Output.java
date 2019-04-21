b136747a8dfa4e7b0c7122e4d73b442314c5f9bb
maghasemzadeh <m.aghasemzadeh@yahoo.com>
2019-04-21 18:18:40 +0430

    Command enum in Controller completed.

---
+++ src/views/Output.java
@@ -0,0 +1,29 @@
+package views;
+
+import models.cards.Hero;
+import models.cards.Minion;
+import models.cards.spell.Spell;
+import models.items.Item;
+
+import java.util.Iterator;
+
+public class Print {
+    // TODO: 4/16/19 Override toString()s
+
+    public static void printHero(Hero hero){
+        System.out.println(hero);
+    }
+
+    public static void printItem(Item item) {
+        System.out.println(item);
+    }
+
+    public static void printMinion(Minion minion) {
+        System.out.println(minion);
+    }
+
+    public static void printSpell(Spell spell) { }
+
+
+
+}
