06ff5d8c5604ed470fd7547d48f1c2383df747cc
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-04-26 20:40:28 +0430

    json parser test

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
-import data.FileReader;
+import com.google.gson.Gson;
+import data.JsonParser;
+import models.cards.Minion;
 import views.menus.AccountMenu;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println(new FileReader().getFileContent(FileReader.MINIONS_DATA));
+        ArrayList<Minion> minions = JsonParser.parseMinion();
+        System.out.println(minions.get(0));
         new AccountMenu().handleMenu();
     }
 }
--- src/data/JsonParser.java
+++ src/data/JsonParser.java
@@ -1,5 +1,18 @@
 package data;
 
+import models.cards.Minion;
+import com.google.gson.Gson;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class JsonParser {
+
+    public static ArrayList<Minion> parseMinion() throws FileNotFoundException {
+        Gson gson = new Gson();
+        String minionsData = new FileReader().getFileContent(FileReader.MINIONS_DATA);
+        
+        return null;
+    }
 
 }
