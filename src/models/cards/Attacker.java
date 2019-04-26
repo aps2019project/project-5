0bff4901ff3026e4acdf20bbc6690ec849f76da3
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-04-26 16:21:10 +0430

    create attacker class.

--- src/models/cards/Attacker.java
+++ src/models/cards/Attacker.java
@@ -2,13 +2,34 @@ package models.cards;
 
 import models.cards.spell.Spell;
 
-public class Attacker extends Card{
-    int health ;
-    int attackPoint ;
-    AttackType attackType ;
-    Spell specialPower ;
+public class Attacker extends Card {
+    private int health;
+    private int range;
+    private int attackPoint;
+    private AttackType attackType;
+    private Spell specialPower;
 
-    public Attacker(String name,String description,int manaPoint,int price) {
+    public Attacker(String name, String description, int manaPoint, int price, int health, int attackPoint, AttackType attackType, int range) {
         super(name, description, manaPoint, price);
+        this.health = health;
+        this.attackPoint = attackPoint;
+        this.attackType = attackType;
+        this.range = range;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public int getRange() {
+        return range;
+    }
+
+    public int getAttackPoint() {
+        return attackPoint;
+    }
+
+    public AttackType getAttackType() {
+        return attackType;
     }
 }
--- src/models/cards/Hero.java
+++ src/models/cards/Hero.java
@@ -1,35 +1,8 @@
 package models.cards;
 
-import models.cards.spell.Spell;
-
-public class Hero extends Card {
-    private int health;
-    private int attackPoint;
-    private AttackType attackType;
-    private int range;
-    private Spell specialPower;
-
-    public int getHealth() {
-        return health;
-    }
-
-    public int getAttackPoint() {
-        return attackPoint;
-    }
-
-    public Spell getSpecialPower() {
-        return specialPower;
-    }
-
-    public void useSpecialPower() {
-
-    }
+public class Hero extends Attacker {
 
     public Hero(String name, String description, int manaPoint, int price, int health, int attackPoint, AttackType attackType, int range) {
-        super(name, description, manaPoint, price);
-        this.health = health;
-        this.attackPoint = attackPoint;
-        this.attackType = attackType;
-        this.range = range;
+        super(name, description, manaPoint, price, health, attackPoint, attackType, range);
     }
 }
