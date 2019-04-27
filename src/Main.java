2385ad6da7f2f7799d105d73da5ad74646fc8309
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-04-27 13:59:57 +0430

    Main class changed

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
-import com.google.gson.Gson;
-import data.JsonParser;
-import models.cards.Minion;
 import views.menus.AccountMenu;
-import java.io.IOException;
-import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        ArrayList<Minion> minions = JsonParser.parseMinion();
-        System.out.println(minions.get(0));
+    public static void main(String[] args) {
         new AccountMenu().handleMenu();
     }
 }
