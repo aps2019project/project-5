2bcfdf7261093cafede50b9236d839f048b1f363
maghasemzadeh <m.aghasemzadeh@yahoo.com>
2019-05-07 01:16:37 +0430

    battle menu commands added completely but may have some bugs(I'll fix tomorrow)

--- src/models/items/CollectableItem.java
+++ src/models/items/CollectableItem.java
@@ -5,9 +5,23 @@ import models.map.Cell;
 public class CollectableItems extends Item {
     private boolean isCollected;
     private CollectableTypes collectableTypes;
+    private String playerName;
+    private int id;
 
     public CollectableItems(String name, Cell cell, String description) {
         super(name, cell, description);
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public void setID(int id) {
+        this.id = id;
+    }
+
+    public String getID() {
+        return String.format("%s_%s_%s", playerName, getName(), id);
     }
 
     public enum CollectableTypes {
