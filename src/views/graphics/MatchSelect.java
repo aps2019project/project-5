64d81d9d384ee7e279d0dc8169caa7b1025800cd
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-13 15:12:56 +0430

    match select image added

--- src/views/graphics/MatchSelect.java
+++ src/views/graphics/MatchSelect.java
@@ -1,9 +1,15 @@
 package views.graphics;
 
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
 import views.Graphics;
 
 public class MatchSelect {
+    public VBox deathMatchContainer;
+    public VBox captureTheFlagContainer;
+    public VBox multiFlagContainer;
+    public VBox ExtraContainer;
+
     public void deathMatch(MouseEvent mouseEvent) {
         Graphics.stage.getScene().setRoot(Graphics.deathMatchRoot);
     }
