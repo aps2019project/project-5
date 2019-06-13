9021f66e7ff0b3eb9ffeb202cea4dd3595ab9036
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-13 08:22:55 +0430

    card gif and back button in shop added

--- src/views/graphics/ShopController.java
+++ src/views/graphics/ShopController.java
@@ -1,17 +1,17 @@
 package views.graphics;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXMasonryPane;
 import controllers.ClientManager;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
 import models.Collection;
 import models.cards.Card;
+import models.cards.Hero;
+import models.cards.Minion;
 
 import java.net.URL;
 import java.util.ArrayList;
@@ -21,6 +21,7 @@ import java.util.ResourceBundle;
 public class ShopController implements Initializable {
 
     public JFXMasonryPane cardContainer;
+    public ImageView backBtn;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -43,12 +44,20 @@ public class ShopController implements Initializable {
             cardPane.getStyleClass().add("card-pane");
             cardPane.setPrefSize(200, 262);
 
-            Label cardName = new Label(card.getName());
-            cardName.relocate(15, 200);
+            Label cardName = new Label(card.getName().toUpperCase());
+            cardName.relocate(15, 140);
             cardName.setPrefWidth(200);
             cardName.setAlignment(Pos.CENTER);
             cardName.getStyleClass().add("card-name-label");
             cardPane.getChildren().add(cardName);
+
+            String type = card instanceof Minion ? "MINION" : (card instanceof Hero ? "HERO" : "SPELL");
+            Label cardType = new Label(type);
+            cardType.relocate(15, 160);
+            cardType.setPrefWidth(200);
+            cardType.setAlignment(Pos.CENTER);
+            cardType.getStyleClass().add("card-type-label");
+            cardPane.getChildren().add(cardType);
 
             cardContainer.getChildren().add(cardPane);
         });
