39cb894c7248b4b7bf7a212d2d76316fee502b48
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-13 14:51:07 +0430

    match select refactored

--- src/views/Graphics.java
+++ src/views/Graphics.java
@@ -55,10 +55,8 @@ public class Graphics extends Application {
     @Override
     public void start(Stage primaryStage) {
         stage = primaryStage;
-//        Fog fog = new Fog(500, 500, new Color(1, 0, 0, 1));
-//        AnchorPane a = new AnchorPane(fog.getView());
 
-        Scene scene = new Scene(shopMenuRoot, 1920, 1080);
+        Scene scene = new Scene(matchSelectRoot, 1920, 1080);
         Image image = new Image("resources/ui/cursor.png");
         scene.setCursor(new ImageCursor(image));
         stage.setFullScreen(true);
