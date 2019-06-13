f2f6165745c51396776f178b9fdd2710e95ce41b
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-13 23:35:23 +0430

    add deck select image

--- src/views/Graphics.java
+++ src/views/Graphics.java
@@ -64,7 +64,7 @@ public class Graphics extends Application {
         loadLayouts();
         stage = primaryStage;
 
-        Scene scene = new Scene(collectionMenuRoot, 1920, 1080);
+        Scene scene = new Scene(mainMenuRoot, 1920, 1080);
         Image image = new Image("resources/images/cursor.png");
         scene.setCursor(new ImageCursor(image));
         stage.setFullScreen(true);
