2818dbbf351de179450b3754129062503ad3b9b3
mahdi5235 <mahdi.haji@yahoo.com>
2019-06-14 13:17:58 +0430

    save and load added to manager

--- src/controllers/Manager.java
+++ src/controllers/Manager.java
@@ -63,11 +63,11 @@ public class Manager {
 
     public static void addAccount(Account account) throws Account.UsernameExistsException {
         Account.getAccounts().clear();
-
-        AccountDataStream.loadAccounts().forEach(account1 -> {
-
-        });
+        ArrayList<Account> toSave = new ArrayList<>();
+        AccountDataStream.loadAccounts().forEach(account1 -> Account.getAccounts().put(account1.getUsername(), account1));
         Account.addAccount(account);
+        Account.getAccounts().forEach((name, account1) -> toSave.add(account1));
+        AccountDataStream.saveAccounts(toSave);
     }
 
     public static void login(String username, String password) throws Account.InvalidPasswordException, Account.InvalidUsernameException {
