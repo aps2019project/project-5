6a7cf1bf52c85f2a8574471689e8ec676af6e53f
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-14 21:55:08 +0430

    create battle menu board

--- src/views/graphics/GraphicBattleController.java
+++ src/views/graphics/GraphicBattleController.java
@@ -1,11 +1,14 @@
 package views.graphics;
 
 import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class GraphicBattleController implements Initializable {
+    public AnchorPane gameBoard;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
