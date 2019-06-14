e2a6c86cd23155984221023162ccc7bd47e7bd59
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-14 20:14:49 +0430

    battle graphics created

---
+++ src/views/graphics/GraphicBattleController.java
@@ -0,0 +1,13 @@
+package views.graphics;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GraphicBattleController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
