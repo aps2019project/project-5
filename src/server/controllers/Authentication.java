6f7ae1e881fe6ecc8624e4a14a7ee3e6eb13aaf5
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-29 22:05:00 +0430

    signup http request and response handled

--- src/server/controllers/Authentication.java
+++ src/server/controllers/Authentication.java
@@ -1,14 +1,19 @@
 package server.controllers;
 
+import com.gilecode.yagson.YaGson;
 import models.Account;
+import models.Response;
 import server.data.DataReader;
 import server.models.Application;
 import server.models.http.HttpRequest;
 import server.models.http.HttpResponse;
+import server.models.http.HttpResponseJSON;
 import server.models.http.HttpResponseText;
+
 import java.util.Map;
 
 public class Authentication extends Application {
+    public static YaGson yaGson = new YaGson();
     public static Map<String, Account> users;
 
     static {
@@ -20,6 +25,20 @@ public class Authentication extends Application {
     }
 
     public static HttpResponse signUp(HttpRequest request) {
-        return new HttpResponseText("signUp");
+        String username = request.GET.get("username");
+        String password = request.GET.get("password");
+        Response response;
+        if(username == null || username.equals("")) {
+            response = new Response(false, "Username can't be null.");
+        } else if(password == null || password.equals("")) {
+            response = new Response(false, "Password can't be null.");
+        } else if(users.containsKey(username)) {
+            response = new Response(false, "This username exists.");
+        } else {
+            Account account = new Account(username, password);
+            users.put(username, account);
+            response = new Response(true, "User created.", account);
+        }
+        return new HttpResponseJSON(yaGson.toJson(response));
     }
 }
