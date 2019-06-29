e898b471be43555b0cc90f126d0cadb292ed7eb8
Ali Shirmohammadi <ali.shirmohammadi2000@gmail.com>
2019-06-29 17:02:20 +0430

    deck added to account model

--- src/models/Account.java
+++ src/models/Account.java
@@ -1,8 +1,14 @@
 package models;
 
+import models.cards.Deck;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Account {
     public String username, password;
-    public String token;
+    public Map<String, Deck> decks = new HashMap<>();
+    public Deck mainDeck;
 
     public Account(String username, String password) {
         this.username = username;
--- src/models/match/Player.java
+++ src/models/match/Player.java
@@ -1,7 +1,9 @@
 package models.match;
 
 import models.Account;
+import models.cards.Deck;
 
 public class Player {
     public Account account;
+    public Deck deck;
 }
