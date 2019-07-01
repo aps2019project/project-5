19ca10838154f9d443135e6d82ae1cf8f44669a1
mahdi5235 <mahdi.haji@yahoo.com>
2019-07-01 14:58:16 +0430

    server shop controller added

---
+++ src/server/controllers/Shop.java
@@ -0,0 +1,48 @@
+package server.controllers;
+
+import com.gilecode.yagson.YaGson;
+import models.Response;
+import server.data.DataReader;
+import server.models.http.HttpRequest;
+import server.models.http.HttpResponse;
+import server.models.http.HttpResponseJSON;
+
+public class Shop {
+    private static YaGson yaGson = new YaGson();
+
+    public static HttpResponse searchShopCards(HttpRequest request) {
+        String searchedContent = request.GET.get("search");
+
+        String cardType = "Card";
+        if (request.GET.containsKey("type")) {
+            cardType = request.GET.get("type");
+        }
+        Class cardClass = null;
+        try {
+            cardClass = Class.forName(cardType);
+        } catch (ClassNotFoundException e) {
+            System.out.println("class not found");
+        }
+        Response response = null;
+        if (request.user == null) {
+            response = new Response(false, "You are not logged in!");
+        } else if (searchedContent.equals("")) {
+            response = new Response(true, "shop cards sent!", DataReader.getShopCollection().cards);
+        } else {
+            response = new Response(true, String.format("search result of %s is sent", searchedContent),
+                    DataReader.getShopCollection().filter(cardClass, searchedContent));
+        }
+        return new HttpResponseJSON(yaGson.toJson(response));
+    }
+
+//    public static HttpResponse getShopCollection(HttpRequest request) {
+//        String token = request.GET.get("token");
+//        Response response = null;
+//        if (!users.containsKey(token)) {
+//            response = new Response(false, "You are not logged in!");
+//        } else {
+//            response = new Response(true, "shop cards sent!", DataReader.getShopCollection().cards);
+//        }
+//        return new HttpResponseJSON(yaGson.toJson(response));
+//    }
+}
