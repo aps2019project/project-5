8b0322412a0d39e7f23b5999838266458eeb7fe6
mahdi5235 <mahdi.haji@yahoo.com>
2019-07-08 09:08:40 +0430

    ChatClient added

---
+++ src/client/controllers/ChatClient.java
@@ -0,0 +1,21 @@
+package client.controllers;
+
+import models.Response;
+
+public class ChatClient {
+    public static String token = AccountClient.user.loginToken;
+
+    public static Response sendMessage(String message) {
+        ServerConnection serverConnection = new ServerConnection("/chat/send_message");
+        serverConnection.parameters.put("token", token);
+        serverConnection.parameters.put("message", message);
+        return serverConnection.getResponse();
+    }
+
+    public static Response update() {
+        ServerConnection serverConnection = new ServerConnection("/chat/update");
+        serverConnection.parameters.put("token", token);
+        return serverConnection.getResponse();
+    }
+
+}
